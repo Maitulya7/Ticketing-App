1918362f5d1459123d39a22aeff5835e601c5a87
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2023-12-23 16:11:51 +0530

    Version 1.0.0

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -2,8 +2,10 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-const TicketForm = () => {
+const TicketForm = ({ticket}) => {
   const router = useRouter();
+
+  const EDITMODE = ticket._id === "new" ? false : true ;
 
   const startingTicketData = {
     title: "",
@@ -13,6 +15,16 @@ const TicketForm = () => {
     status: "Not Started",
     category: "Software Developer",
   };
+
+  if (EDITMODE) {
+    startingTicketData["title"] = ticket.title;
+    startingTicketData["description"] = ticket.description;
+    startingTicketData["priority"] = ticket.priority;
+    startingTicketData["progress"] = ticket.progress;
+    startingTicketData["status"] = ticket.status;
+    startingTicketData["category"] = ticket.category;
+  }
+
 
   const [formData, setFormData] = useState(startingTicketData);
 
@@ -28,19 +40,33 @@ const TicketForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("/api/Tickets", {
-      method: "POST",
-      body: JSON.stringify({ formData }),
-      "content-type": "application/json",
-    });
 
-    if (!res.ok) {
-      throw new Error("Failed to create Ticket");
+    if (EDITMODE) {
+      const res = await fetch(`/api/Tickets/${ticket._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ formData }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update ticket");
+      }
+    } else {
+      const res = await fetch("/api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({ formData }),
+        "Content-Type": "application/json",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to create ticket");
+      }
     }
-
-    router.refresh();
     router.push("/");
+    router.refresh();
+ 
   };
+
 
   return (
     <div className="flex justify-center">
@@ -49,7 +75,7 @@ const TicketForm = () => {
         method="post"
         onSubmit={handleSubmit}
       >
-        <h3>Create Ticket</h3>
+        <h3>{EDITMODE ? "Update Your Ticket" : "Create New Ticket"}</h3>
         <label>Title</label>
         <input
           type="text"
@@ -150,7 +176,11 @@ const TicketForm = () => {
           <option value="Done">Done</option>
         </select>
 
-        <input type="submit" className="btn" value="Create Ticket" />
+        <input
+          type="submit"
+          className="btn max-w"
+          value={EDITMODE ? "Update Ticket" : "Create Ticket"}
+        />
       </form>
     </div>
   );
--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,9 +1,35 @@
 import TicketForm from "@/app/(components)/TicketForm"
 
-function TicketPage({params}) {
+const getTicketById = async (id) =>{
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`,{
+    cache:"no-store",
+  })
+
+  if(!res.ok){
+    throw new Error("Could not find ticket.")
+  }
+
+  return res.json();
+};
+
+const  TicketPage = async ({params})=> {
+
+  const EDITMODE = params.id === "new" ? false : true;
+  let updateTicketData = {};
+
+  if(EDITMODE){
+    updateTicketData = await getTicketById(params.id);
+    updateTicketData = updateTicketData.foundTicket;
+    console.log(updateTicketData);
+
+  } else{
+    updateTicketData ={
+      _id:"new",
+    }
+  }
   return (
     <>
-        <TicketForm/>    
+        <TicketForm ticket={updateTicketData}/>    
     </>
   )
 }
